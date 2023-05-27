81958e6b2862f3442ac484abce9b15728be24c46
Sergey9247 <Sammakarov@list.ru>
2023-05-27 13:53:10 +0300

    com

---
+++ src/main/java/ru/kata/spring/boot_security/demo/security/RoleGrantedAuthority.java
@@ -0,0 +1,19 @@
+package ru.kata.spring.boot_security.demo.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import ru.kata.spring.boot_security.demo.model.Role;
+
+public class RoleGrantedAuthority implements GrantedAuthority {
+
+
+    private final Role role;
+
+    public RoleGrantedAuthority(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String getAuthority() {
+        return role.getRoleName();
+    }
+}
